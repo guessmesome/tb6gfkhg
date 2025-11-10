350fae11cb75cf7e061dacdbd111eab1ad9df2a7
nn <205666415+guessmesome@users.noreply.github.com>
2025-11-10 14:48:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const productLinks = {
-    product1: "https://trnsfx.ru/go/shcj8yfbdz",
-    product2: "https://trnsfx.ru/go/s0uusze41d",
-    product3: "https://trnsfx.ru/go/sh5ztmjczt",
-    product4: "https://trnsfx.ru/go/s2s0j4ozv5"
+    product1: "https://vldmnt.ru/go/suwmezz59p",
+    product2: "https://vldmnt.ru/go/s6u0wsykcc",
+    product3: "https://vldmnt.ru/go/svo7592hmj",
+    product4: "https://vldmnt.ru/go/sx1411qdrd"
 };
 
